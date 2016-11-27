19082c3ddc31b19314334f2929b4f4efbbc491cb
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-11-27 22:47:49 +0000

    adding default routes

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,13 @@ module.exports = (req, res) => {
   switch (req.url) {
     case '/get': return increment(req, res, sendResponse.bind(this, res));
     case '/status': return status(req, res, sendResponse.bind(this, res));
+    default: return notFound(req, res);
   }
+}
+
+function notFound(request, response) {
+  response.writeHead(404, {"Content-Type": "application/json" });
+  response.end(`{"result": "unknown endpoint"}`);
 }
 
 function sendResponse(response, error, result) {
@@ -15,7 +21,7 @@ function sendResponse(response, error, result) {
     console.error(error);
   }
   response.writeHead(httpCode, {"Content-Type": "application/json" });
-  response.end(`{"result": "${result}"}`);
+  response.end(`{"result":${result}}`);
 }
 
 process.on('uncaughtException', (error) => {
